aafa2e87556444d96addb19f683767c8fbc7a77b
Bharathwaj RR <rrbharathwaj@yahoo.co.in>
2014-03-13 21:50:55 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CAMS::Application.config.secret_key_base = 'daf6453739646cace5c2bb53edfd359cdcf2addab71f874af70c612713a14b9b55faf53910539f28ca4aa3e52276ee9f818053d3d5fd5c75126bddd41a8078d1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CAMS::Application.config.session_store :cookie_store, key: '_CAMS_session'
